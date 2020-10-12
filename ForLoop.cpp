35e1e5c3555d7a7dafa6021c6ac3057d0b63ede1
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:09:15 +0530

    Program to demonstrate FOR LOOP in cpp

---
+++ ForLoop.cpp
@@ -0,0 +1,22 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate FOR LOOP in cpp.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int i=0;
+    cout<<"\n\n\t Demonstration of FOR LOOP in C++ >> ";
+    for(i=1;i<=5;i++){
+        cout<<" "<<i;
+    }
+    return 0;
+}
